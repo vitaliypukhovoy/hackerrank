0daac1b8a4ed74e7cf3d4d87af6991cc5e7f26e4
vitah <vitaliypukhovoy@gmail.com>
2018-03-16 19:10:21 +0200

    new solution

---
+++ hackerrank/Majority50PerentItemOfArray/Program.cs
@@ -0,0 +1,54 @@
+﻿sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majority50PerItemOfArray
+{
+    class Program
+    {
+        //Given an unsorted array which has a number in the majority (a number appears more than 50% 
+        //in the array), find that number?      
+        static void Main(string[] args)
+        {
+
+            var arr =  new int [] {1,2,2,3,2,6,8,9,2,2,2,2,3,4,5};            
+            var half = arr.Length/2;
+            var dic = new Dictionary<int, int>();            
+            foreach(var i in arr)
+            {
+                if (dic.ContainsKey(i))
+                { 
+                int newValue = dic[i]++;
+                //AddOrUpdate(dic, i, newValue);                
+                    //if (dic.Max(k => k.Value)>= half)
+                    if(dic[i] >= half)
+                    break;
+                }
+                else 
+                {
+                    dic.Add(i, 1);
+                    //AddOrUpdate(dic, i, 1); 
+                }
+            }
+                Console.WriteLine(dic.SingleOrDefault(i => i.Value == dic.Max(k => k.Value)).Key);
+                Console.WriteLine(dic.Max(i=>i.Value));
+                Console.ReadKey();
+        }
+
+        public static void AddOrUpdate(Dictionary<int, int> dic, int key, int newValue)
+        {
+           int val;         
+           if( dic.TryGetValue(key, out val))
+           {
+               dic[key] = newValue;
+           }
+           else
+           {
+               dic.Add(key, newValue);
+           }                
+        
+        }
+    }
+}
---
+++ hackerrank/Queue using Two Stacks/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue_using_Two_Stacks
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            Stack<int> st1 = new Stack<int>();
+            Stack<int> st2 = new Stack<int>();
+            List<List<int>> list = new List<List<int>>();
+
+            string line;
+            while ((line = Console.ReadLine()) != null && line != null)
+            {
+                list.Add(line.Split(' ').Select(Int32.Parse).ToList());
+            }
+
+            list.Reverse();
+
+            foreach (var item in list)
+            {
+                st1.Push(item[0]);
+                if (item.FindIndex(i => i == 1) != -1)
+                    st2.Push(item[1]);
+                else
+                    st2.Push(0);
+            }
+
+            st1.Pop();
+            st2.Pop();
+
+            //  int tmp = 0; ;
+            for (int i = 0; i < st1.Count; i++)
+            {
+
+                if (st1.ElementAt(i) == 1)
+                {
+                    //  tmp = st2.Peek(); 
+                    //     st1.Pop();
+                }
+                else if (st1.ElementAt(i) == 2)
+                {
+                    st2.Pop();
+                    st2.Pop();
+                    st1.Pop();
+                    // st1.Pop();
+
+                }
+                else if (st1.ElementAt(i) == 3)
+                {
+                    Console.WriteLine(st2.Peek());
+                    st1.Pop();
+                    // Console.WriteLine(st1.ElementAt(i) + " " + st2.ElementAt(i));
+                    // st1.Pop();
+                }
+                // Console.WriteLine(st1.ElementAt(i));
+            }
+
+            //  foreach(var el in st2)
+            //       Console.WriteLine(el);
+
+        }
+    }
+}
